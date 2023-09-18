34fb96030a0aea754af6604db913e6b291d414c7
Allen <you@example.com>
2023-09-17 22:59:34 -0400

    added Multiples and Reduce

---
+++ src/Reduce.java
@@ -0,0 +1,17 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int steps = 0;
+
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n /= 2; // If n is even, divide it by 2
+            } else {
+                n -= 1; // If n is odd, subtract 1
+            }
+            steps++;
+        }
+
+        System.out.println("Number of steps to reach 0 starting from 100: " + steps);
+    }
+}
